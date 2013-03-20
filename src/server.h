e809375a63d669c3a3e5142a0d37627458d2a684
Kyle Falconer <kfalconer@gmail.com>
2013-03-20 18:55:29 -0500

    added server.h
    Intented to be a unified structure for standardized communication
    between server functions.

---
+++ src/server.h
@@ -0,0 +1,17 @@
+
+struct player {
+	int points;
+	int color;		// 0-9 preset colors
+	char* username;
+	int portnumber;	// do we need this?
+	bool connected;	// whether or not the client is currently connected
+	bool ready;		// whether or not the client is ready for a round
+}
+
+struct serverinfo {
+	struct player* players;
+	int num_players;
+	char* base_word;			// the root word
+	char* base_word_sorted;		// base_word with letters in alphabetic order
+	char* base_word_factors;	// the list of words able to be made from base_word, in alphabetic order
+}
